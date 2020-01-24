f01cf562f2bd654c9e68c2cdc15e13bbec9a3fef
flash <boutiri.radouane@gmail.com>
2020-01-23 21:32:09 -0500

    adding project

---
+++ partie1-javascript/monnaies.js
@@ -0,0 +1,19 @@
+var MONNAIES = [
+	{	nom: "Euro",valeur: 1.5		}, 
+	{	nom: "$US", valeur: 1.2		}, 
+	{	nom: "GBP",	valeur: 1.7		}, 
+	{	nom: "CHF",	valeur: 1.3		}, 
+	{	nom: "CNY",	valeur: 0.2		}, 
+	{	nom: "TRY",	valeur: 0.33	}
+];
+var listeMonnaies = {
+	data : MONNAIES,
+	getNomsMonnaies : function() {
+		//À COMPLÉTER :
+		
+	},
+	getTaux : function(nomMonnaie) {
+		//À COMPLÉTER :
+		
+	}
+};
